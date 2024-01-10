c523fec2b00931be6e0d522a56276086c57e1a26
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-10 16:38:21 -0700

    test: intialize Deck

---
+++ spec/deck_spec.rb
@@ -0,0 +1,16 @@
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+  let!(:card1) { Card.new(:diamond, 'Queen', 12) }
+  let!(:card2) { Card.new(:spade, '3', 3) }
+  let!(:card3) { Card.new(:heart, 'Ace', 14) }
+  let!(:cards) { [card1, card2, card3] }
+  let!(:deck) { Deck.new(cards) }
+
+  describe "#initialize" do
+    it "exists" do
+      expect(deck).to be_an_instance_of(Deck)
+    end
+  end
+end
