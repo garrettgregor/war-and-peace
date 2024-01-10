98b0ae6577adcaa9ca52e93137a59eb5b2ef3828
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-10 16:39:19 -0700

    test: rank_of_card_at

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,12 +5,22 @@ RSpec.describe Deck do
   let!(:card1) { Card.new(:diamond, 'Queen', 12) }
   let!(:card2) { Card.new(:spade, '3', 3) }
   let!(:card3) { Card.new(:heart, 'Ace', 14) }
+  let!(:card4) { Card.new(:club, '5', 5) }
+
   let!(:cards) { [card1, card2, card3] }
   let!(:deck) { Deck.new(cards) }
 
   describe "#initialize" do
     it "exists" do
       expect(deck).to be_an_instance_of(Deck)
+      expect(deck.cards).to eq(cards)
+    end
+  end
+
+  describe "#rank_of_card_at" do
+    it "returns the rank of a card a specified index" do
+      expect(deck.rank_of_card_at(0)).to eq(12)
+      expect(deck.rank_of_card_at(2)).to eq(14)
     end
   end
 end
