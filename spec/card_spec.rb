f6716c0c882fbd083ebea226190c57ad5620cb25
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-10 16:37:18 -0700

    test: add card with suit, value and rank

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,14 @@
 require "./lib/card"
 
 RSpec.describe Card do
-  
+  let!(:card) { Card.new(:heart, 'Jack', 11) }
+
+  describe "instance methods" do
+    it "intializes" do
+      expect(card).to be_an_instance_of(Card)
+      expect(card.suit).to eq(:heart)
+      expect(card.value).to eq("Jack")
+      expect(card.rank).to eq(11)
+    end
+  end
 end
