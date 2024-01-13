5b2f0f4b7e4e2ac33609d694e9bdf7ea73fb846d
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-12 23:57:43 -0700

    test: mutually_assured_destruction

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -103,4 +103,44 @@ RSpec.describe Turn do
       end
     end
   end
+
+  describe "type :mutually_assured_destruction" do
+    let(:card1) { Card.new(:heart, 'Jack', 11) }
+    let(:card2) { Card.new(:heart, '10', 10) }
+    let(:card3) { Card.new(:heart, '9', 9) }
+    let(:card4) { Card.new(:diamond, 'Jack', 11) }
+    let(:card5) { Card.new(:heart, '8', 8) }
+    let(:card6) { Card.new(:diamond, '8', 8) }
+    let(:card7) { Card.new(:heart, '3', 3) }
+    let(:card8) { Card.new(:diamond, '2', 2) }
+    let(:deck1) { Deck.new([card1, card2, card5, card8]) }
+    let(:deck2) { Deck.new([card4, card3, card6, card7]) }
+    let(:player1) { Player.new("Megan", deck1) }
+    let(:player2) { Player.new("Aurora", deck2) }
+    let(:turn) { Turn.new(player1, player2) }
+
+    context "#winner" do
+      it "returns the winner of the turn" do
+        expect(turn.type).to eq(:mutually_assured_destruction)
+        expect(turn.winner).to eq("No Winner")
+      end
+    end
+
+    context "#pile_cards" do
+      it "will have each player will send one card (the top card) to the spoils pile" do
+        turn.pile_cards
+
+        expect(turn.spoils_of_war).to eq([])
+      end
+    end
+
+    context "#award_spoils" do
+      it "adds each of the cards in the spoils_of_war array to the winner of the turn" do
+        turn.pile_cards
+
+        expect(player1.deck.cards).to eq([card8])
+        expect(player2.deck.cards).to eq([card7])
+      end
+    end
+  end
 end
