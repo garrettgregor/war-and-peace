b4c787c308d989b730b7746b09b9c5e33177234a
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-16 11:32:07 -0700

    refactor: use deck methods and shuffle spoils of war

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,42 +9,41 @@ class Turn
   end
 
   def type
-    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-      :basic
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
       player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       :war
+    elsif player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
     end
   end
 
   def winner
     case type
-    when :basic
-      players.max_by { |player| player.deck.rank_of_card_at(0) }
-    when :war
-      players.max_by { |player| player.deck.rank_of_card_at(2) }
     when :mutually_assured_destruction
       "No Winner"
+    when :war
+      players.max_by { |player| player.deck.rank_of_card_at(2) }
+    when :basic
+      players.max_by { |player| player.deck.rank_of_card_at(0) }
     end
   end
 
   def pile_cards
     case type
-    when :basic
-      players.each { |player| spoils_of_war << player.deck.cards.shift }
-    when :war
-      players.each { |player| spoils_of_war << player.deck.cards.shift(3) }
-      spoils_of_war.flatten!
     when :mutually_assured_destruction
-      players.each { |player| player.deck.cards.shift(3) }
+      players.each { |player| 3.times { player.deck.remove_card } }
+    when :war
+      players.each { |player| 3.times { spoils_of_war << player.deck.remove_card } }
+    when :basic
+      players.each { |player| spoils_of_war << player.deck.remove_card }
     end
   end
 
   def award_spoils(winner)
     while !spoils_of_war.empty?
-      winner.deck.cards << spoils_of_war.pop
+      winner.deck.cards << spoils_of_war.shuffle!.pop
     end
   end
 end
